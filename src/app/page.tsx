e79a306b8a82ecbf27e36d890ca6e743e08d6091
Zachary King <zak@delfina.com>
2024-05-21 11:18:48 -0700

    second first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      The personal website of <span className="font-bold">Zak King</span>
+    </main>
+  );
+}
