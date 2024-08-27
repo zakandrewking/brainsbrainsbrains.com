2dc9ea33235b9a7ed815da5b369826bc94a5c6fe
Zachary King <zak@delfina.com>
2024-08-27 11:22:10 -0700

    favicon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col mt-20 text-muted-foreground">
       <div>Credits:</div>
-      <a href="https://github.com/twitter/twemoji">Emoji Favicon</a>
+      <a href="https://favicon.io/emoji-favicons/goggles">Emoji Favicon</a>
       <a href="https://heredragonsabound.blogspot.com/2020/02/creating-pencil-effect-in-svg.html">
         Pencil effects
       </a>
