e6911593102f5d52d93ae764725845cddceca36d
Zachary King <zak@delfina.com>
2024-11-07 15:51:26 -0800

    world sim

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -4,19 +4,12 @@ import * as runtime from "react/jsx-runtime";
 
 import { run } from "@mdx-js/mdx";
 
-import { useMDXComponents } from "../../mdx-components";
+import { getMDXComponents } from "../../mdx-components";
 import { getSortedPostsData } from "../util/blog-util";
 import { H3 } from "./ui/typography";
 
 export default async function PostList({ tag }: { tag?: string }) {
   const allPostsData = await getSortedPostsData();
-
-  const mdxString = allPostsData.posts[0].preview;
-
-  const { default: MDXContent } = await run(mdxString, {
-    ...(runtime as any),
-    baseUrl: join(import.meta.url, ".."),
-  });
 
   const filteredPosts = tag
     ? allPostsData.posts.filter((post) => {
@@ -25,28 +18,24 @@ export default async function PostList({ tag }: { tag?: string }) {
       })
     : allPostsData.posts;
 
-  return filteredPosts.map(({ id, date, title, anchor }) => (
-    <div className="pb-8">
-      <div className="text-gray-600 dark:text-gray-400 mb-2">
-        {date.toDateString()}
+  return filteredPosts.map(async ({ id, date, title, anchor, preview }) => {
+    const { default: MDXContent } = await run(preview, {
+      ...(runtime as any),
+      baseUrl: join(import.meta.url, ".."),
+    });
+    return (
+      <div className="pb-8">
+        <div className="text-gray-600 dark:text-gray-400 mb-2">
+          {date.toDateString()}
+        </div>
+        <Link href={`/blog/${id}`} key={id}>
+          <H3 underline={true} className="text-black dark:text-white mb-1">
+            {title}
+          </H3>
+        </Link>
+        <MDXContent components={getMDXComponents({})} />
+        <Link href={`/blog/${id}#${anchor}`}>[ Keep reading ]</Link>
       </div>
-      <Link href={`/blog/${id}`} key={id}>
-        <H3 underline={true} className="text-black dark:text-white mb-1">
-          {title}
-        </H3>
-      </Link>
-      <MDXContent components={useMDXComponents({})} />
-      <Link href={`/blog/${id}#${anchor}`}>[ Keep reading ]</Link>
-      {/* <div>
-        Tags:{" "}
-        {tags
-          .map((tag) => <Link href={`/tag/${tag}`}>{tag}</Link>)
-          .reduce((prev, curr) => (
-            <>
-              {prev}, {curr}
-            </>
-          ))}
-      </div> */}
-    </div>
-  ));
+    );
+  });
 }
