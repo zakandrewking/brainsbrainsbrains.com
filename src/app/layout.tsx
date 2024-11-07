0002284cd77afa8ab3cab30277a92f6a12669db0
Zachary King <zak@delfina.com>
2024-11-07 15:29:07 -0800

    scroll fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           </head>
           <body
             className={cn(
-              "min-h-screen bg-background font-sans antialiased",
+              "min-h-screen bg-background font-sans antialiased overflow-scroll",
               fontSans.variable
             )}
           >
